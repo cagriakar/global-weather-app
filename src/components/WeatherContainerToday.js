fcbc39518a00b5e5ba22a8671168199480d59d79
cagriakar <55461100+cagriakar@users.noreply.github.com>
2020-05-07 16:19:08 +0300

    mobile version adjustment

--- src/components/WeatherContainerToday.js
+++ src/components/WeatherContainerToday.js
@@ -1,30 +1,30 @@
-import React, { useState, useEffect, useContext } from "react";
-import { getWeatherToday } from "../calls/weatherCall";
-import { Container } from "react-bootstrap";
-import WeatherRow from "./WeatherRow";
-import { URLContext } from "../contexts/URLContext";
+import React, { useState, useEffect, useContext } from 'react';
+import { getWeatherToday } from '../calls/weatherCall';
+import { Container } from 'react-bootstrap';
+import WeatherRow from './WeatherRow';
+import { URLContext } from '../contexts/URLContext';
 
 function WeatherContainerToday() {
-  const [hoursList, setHoursList] = useState([]);
-  // eslint-disable-next-line no-unused-vars
-  const [url, setUrl] = useContext(URLContext);
+	const [hoursList, setHoursList] = useState([]);
+	// eslint-disable-next-line no-unused-vars
+	const [url, setUrl] = useContext(URLContext);
 
-  useEffect(() => {
-    getWeatherToday(url)
-      .then(response => {
-        setHoursList(response.today);
-      })
-      .catch(error => console.log(error));
-    return () => {};
-  }, [url]);
+	useEffect(() => {
+		getWeatherToday(url)
+			.then((response) => {
+				setHoursList(response.today);
+			})
+			.catch((error) => console.log(error));
+		return () => {};
+	}, [url]);
 
-  return (
-    <Container className="px-auto mx-auto">
-      {hoursList.map((hour, index) => (
-        <WeatherRow key={index} data={hour}></WeatherRow>
-      ))}
-    </Container>
-  );
+	return (
+		<Container className="px-auto mx-auto">
+			{hoursList?.map((hour, index) => (
+				<WeatherRow key={index} data={hour}></WeatherRow>
+			))}
+		</Container>
+	);
 }
 
 export default WeatherContainerToday;
